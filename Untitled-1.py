d43b7b2c5450b3daa32be3328f350807633e4229
yusithiesun <50345176+yusithiesun@users.noreply.github.com>
2019-05-12 18:42:02 +0800

    Add files via upload
    终于写出来啦！！！！

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+import time
+with open("C:\\Users\\28347\\Desktop\\5.9python\\text-file-process-yusithiesun\\text-file-process\\log_files\\201811113003.log",encoding='utf-8')as f:
+  counter=0
+  for line in f:
+    student_id=line.split(',')[1]
+    student_id=student_id.strip()
+    print(student_id)
+    if student_id[3:]=='201811113003':
+      counter+=1
+  print(counter)
+time.sleep(60)
